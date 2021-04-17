a143119a8889a5912efee256c8fefb4b33542eff
Kristian Salonen <github@krisu.anonaddy.com>
2021-04-17 18:15:57 +0300

    SVGO batch script and renaming BATs

---
+++ _compress/.0_svgo_prettify-svg.bat
@@ -0,0 +1,10 @@
+:: MAKE SURE YOU
+:: CREATED (svg_input)
+:: FOLDER AND ADDED
+:: SVGS THERE
+
+svgo --multipass --pretty --indent 3 -f "%cd%\(svg_input)" -o "%cd%"
+
+:: In case when SVGO fails, open CMD.exe to this directory and execute:
+::   call .0_svgo_prettify-svg.bat
+:: This shows will show the errors, when just using BAT normally doesn't
---
+++ _compress/.1_remove_(svg_input)_files.bat
@@ -0,0 +1,3 @@
+@echo off
+cd (svg_input)
+del *.*
