2b96aded2ba3caa20ea8848c3ff3467492fc90fa
Kristian Salonen <github@krisu.anonaddy.com>
2021-06-04 01:23:37 +0300

    "Edit JSONs on Notepad++" function for "Icon Pack Creator" BAT

--- _compress/.5_icon_pack_creator.bat
+++ _compress/.5_icon_pack_creator.bat
@@ -14,22 +14,36 @@ echo ---------------------------------
 echo.
 echo Choose:
 echo [1] Generate JSON
-echo [2] Make the iconpack ZIP (JSON has to generated first)
-echo [3] Zip the PNG files
-echo [4] Exit
+echo [2] Edit JSONs on Notepad++
+echo [3] Make the iconpack ZIP (JSON has to generated first)
+echo [4] Zip the PNG files
+echo [5] Exit
 echo.
-choice /c 1234 /n /m "Chosen input (1-4): "
+choice /c 12345 /n /m "Choose (1-5): "
 echo.
 
-if errorlevel 4 goto exitScript
-if errorlevel 3 goto zipPNGs
-if errorlevel 2 goto makeSVGzip
+if errorlevel 5 goto exitScript
+if errorlevel 4 goto zipPNGs
+if errorlevel 3 goto makeSVGzip
+if errorlevel 2 goto editJsons
 if errorlevel 1 goto generateJson
 
 :generateJson
 echo - - - - - - - - - - - - - - - - -
 echo.
 pack.py gen-def
+echo.
+echo ---------------------------------
+goto loop
+
+:editJsons
+echo - - - - - - - - - - - - - - - - -
+echo.
+:: 32-bit version of Notepad++ has to be installed
+echo Opening "pack.json" to Notepad++ ...
+start "" /b "%ProgramFiles(x86)%\Notepad++\notepad++.exe" "%cd%\pack.json"
+echo Opening "pack_TEMPLATE.json" ^& "pack.json" files to Notepad++ (new session for compare plugin) ...
+start "" /b "%ProgramFiles(x86)%\Notepad++\notepad++.exe" -multiInst -nosession "%cd%\pack_TEMPLATE.json" "%cd%\pack.json"
 echo.
 echo ---------------------------------
 goto loop
@@ -44,7 +58,7 @@ goto loop
 
 :zipPNGs
 echo - - - - - - - - - - - - - - - - -
-:: line-break by 7za
+:: line-break by 7-Zip CMD
 7za a -tzip "%cd%\aegis-icons_PNGs_v%version%_legacy.zip" "%cd%\PNG\*"
 echo.
 echo ---------------------------------
