7893865a7395017401126177d5b262f41e0bbd9e
Kristian Salonen <github@krisu.anonaddy.com>
2021-06-25 20:23:25 +0300

    New icons: Adafruit (+ var), Discogs (+ vars), SOGo
    #281, #280, #282

--- _compress/.0_svgo_prettify-svg.bat
+++ _compress/.0_svgo_prettify-svg.bat
@@ -1,10 +1,14 @@
-:: MAKE SURE YOU
-:: CREATED (svg_input)
-:: FOLDER AND ADDED
-:: SVGS THERE
+:: ADD SVGs TO
+:: THIS DIRECTORY
+:: WHERE THIS BAT
+:: FILE EXIST
+
+@echo off
+mkdir (svg_input)
+robocopy "%cd%" "%cd%\(svg_input)" *.svg /move
 
 svgo --multipass --pretty --indent 3 -f "%cd%\(svg_input)" -o "%cd%"
 
-:: In case when SVGO fails, open CMD.exe to this directory and execute:
-::   call .0_svgo_prettify-svg.bat
+:: In case when SVGO fails, open CMD.exe to (svg_input) directory and execute:
+::   svgo --multipass --pretty --indent 3 -f "%cd%" -o "%cd%\(debug)"
 :: This shows the errors, when just using BAT normally doesn't
