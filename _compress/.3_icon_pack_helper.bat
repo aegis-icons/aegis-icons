ba414b6299a571e9b8017be03831dd518878929a
Kristian Salonen <github@krisu.anonaddy.com>
2021-08-13 03:50:25 +0300

    Icon Pack Helper: dry run now adds date to filename and moves it to trash bin instead just deleting it

--- _compress/.3_icon_pack_helper.bat
+++ _compress/.3_icon_pack_helper.bat
@@ -1,12 +1,16 @@
 @echo off
 title Icon Pack Helper for aegis-icons
 cd ..
-setlocal EnableDelayedExpansion
-for /f "skip=1 tokens=1-6" %%a in ('wmic path win32_localtime get day^,hour^,minute^,month^,second^,year /format:table') do (
-    if "%%b" neq "" (
-        set /a version=%%f*10000+%%d*100+%%a
-    )
+for /f "skip=1 tokens=1-6" %%g in ('wmic path win32_localtime get day^,hour^,minute^,month^,second^,year /format:table') do (
+  if "%%~l"=="" goto s1_done
+  set _yyyy=%%l
+  set _mm=00%%j
+  set _dd=00%%g
 )
+:s1_done
+set _mm=%_mm:~-2%
+set _dd=%_dd:~-2%
+set version=%_yyyy%%_mm%%_dd%
 echo.
 echo ---------------------------------
 echo ------- ICON PACK HELPER --------
@@ -15,7 +19,7 @@ echo ---------------------------------
 echo.
 echo [1] Generate JSON
 echo [2] Edit JSONs on Notepad++
-echo [3] Dry run-ish (generate ^& delete icon pack ZIP)
+echo [3] Dry run-ish (generate ^& move icon pack ZIP to trash bin)
 echo [4] Make the icon pack ZIP (JSON has to generated first)
 echo [5] Make pack ZIP with manually added version
 echo [6] Zip the PNG files
@@ -53,8 +57,19 @@ goto loop
 :dryRunish
 echo - - - - - - - - - - - - - - - - -
 echo.
-pack.py gen --output aegis-icons.zip --version %version%
-del aegis-icons.zip
+for /f "skip=1 tokens=1-6" %%g in ('wmic path win32_localtime get day^,hour^,minute^,month^,second^,year /format:table') do (
+if "%%~l"=="" goto s2_done
+  set _hour=00%%h
+  set _minute=00%%i
+)
+:s2_done
+set _hour=%_hour:~-2%
+set _minute=%_minute:~-2%
+set _second=%time:~-5,2%
+set dryFileName=aegis-icons_-_dryrun_[%_yyyy%-%_mm%-%_dd%_%_hour%.%_minute%.%_second%].zip
+pack.py gen --output %dryFileName% --version %version%
+:: NirCmd has be in the folder that's added to the PATH <https://www.nirsoft.net/utils/nircmd.html>
+nircmd moverecyclebin %dryFileName%
 echo.
 echo ---------------------------------
 goto loop
